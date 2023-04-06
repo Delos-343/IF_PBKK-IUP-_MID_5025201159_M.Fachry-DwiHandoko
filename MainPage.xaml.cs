be4b258bde351061c3c839ff637d5d56fa6ebb35
Delos-343 <mfachry.handoko@gmail.com>
2023-04-06 14:34:05 +0700

    added BMI Calculator, SQLite DB error

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,59 @@
-﻿namespace BMI_Calc_App;
+﻿using SQLite;
+using System;
+using System.IO;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
-public partial class MainPage : ContentPage
+namespace BMI_Calc_App
 {
-	int count = 0;
+    public partial class MainPage : ContentPage
+    {
+        private SQLiteAsyncConnection connection;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+        }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        private async void InitializeDatabase()
+        {
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "bmi.db");
+            connection = new SQLiteAsyncConnection(databasePath);
+            await connection.CreateTableAsync<BMIRecord>();
+        }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        private void OnCalculateBMI(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(weightEntry.Text) && !string.IsNullOrWhiteSpace(heightEntry.Text))
+            {
+                var weight = double.Parse(weightEntry.Text);
+                var height = double.Parse(heightEntry.Text) / 100;
+                var bmi = weight / (height * height);
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+                resultLabel.Text = $"Your BMI is {bmi:F1}";
+
+                var record = new BMIRecord
+                {
+                    Weight = weight,
+                    Height = height,
+                    BMI = bmi,
+                    Date = DateTime.Now
+                };
+
+                connection.InsertAsync(record);
+            }
+        }
+    }
+
+    public class BMIRecord
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public double BMI { get; set; }
+        public DateTime Date { get; set; }
+    }
 }
 
